44582bfe2b66c6bddfd30585c4e3a1676ac9e8bf
globulous <r.pitot@laposte.net>
2024-04-17 16:30:23 +0200

    up

--- Assets/Scripts/QuestManager.cs
+++ Assets/Scripts/QuestManager.cs
@@ -4,19 +4,20 @@ using UnityEngine;
 
 public class QuestManager : MonoBehaviour
 {
-    [SerializeField] public List<Transform> questPos = new List<Transform>();
-    [SerializeField] public List<GameObject> questList = new List<GameObject>();
+    [SerializeField] public List<Transform> positions = new List<Transform>();
+    [SerializeField] public List<GameObject> quests = new List<GameObject>();
 
     void Start()
     {
-        List<Transform> availablePos = new List<Transform>(questPos);
-        for (int i = 0; i < questList.Count; i++)
+        List<Transform> availablePos = new List<Transform>(positions);
+
+        for (int i = 0; i < quests.Count; i++)
         {
-            int randomIndex = Random.Range(0, questPos.Count);
+            int randomIndex = Random.Range(0, quests.Count);
 
             Transform spawnTransform = availablePos[randomIndex];
 
-            Instantiate(questList[i], spawnTransform.position, Quaternion.identity);
+            Instantiate(quests[i], spawnTransform.position, Quaternion.identity);
 
             availablePos.RemoveAt(randomIndex);
         }
