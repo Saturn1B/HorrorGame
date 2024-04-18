46ec4393c5eeafce4a7de8f46b59bbb816f3100b
globulous <r.pitot@laposte.net>
2024-04-18 11:02:16 +0200

    up

--- Assets/Scripts/QuestManager.cs
+++ Assets/Scripts/QuestManager.cs
@@ -14,17 +14,9 @@ public class QuestManager : MonoBehaviour
 
         foreach (GameObject quest in quests)
         {
-            // Vérifie s'il reste des positions disponibles
-            if (availablePos.Count == 0)
-            {
-                Debug.LogWarning("Il n'y a plus de positions disponibles pour instancier les quêtes.");
-                break; // Sort de la boucle si toutes les positions ont été utilisées
-            }
-
             int randomIndex;
             Transform spawnTransform;
 
-            // Trouve un index non utilisé
             do
             {
                 randomIndex = Random.Range(0, availablePos.Count);
@@ -34,7 +26,6 @@ public class QuestManager : MonoBehaviour
             spawnTransform = availablePos[randomIndex];
             usedIndexes.Add(randomIndex);
 
-            // Instancie la quête
             Instantiate(quest, spawnTransform.position, Quaternion.identity);
         }
     }
