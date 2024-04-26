4401556e730a5693bd70c8ee3253e4fba7b28b32
Saussol <60229019+Saussol@users.noreply.github.com>
2024-04-26 12:42:15 +0200

    ae

--- Assets/Scripts/Steamworks.NET/Lobby/PlayerNameSync.cs
+++ Assets/Scripts/Steamworks.NET/Lobby/PlayerNameSync.cs
@@ -33,7 +33,19 @@ public class PlayerNameSunc : NetworkBehaviour
     private void OnDisplayNameChanged(string oldName, string newName)
     {
         // Mettre à jour l'affichage du pseudonyme dans l'interface utilisateur, par exemple
-        Debug.Log("Display Name Changed: " + newName);
+        //Debug.Log("Display Name Changed: " + newName);
+
+        if (isLocalPlayer)
+        {
+            // Afficher le pseudonyme du joueur local
+            Debug.Log("My Display Name: " + newName);
+        }
+        else
+        {
+            // Afficher le pseudonyme du joueur distant
+            Debug.Log("Friend's Display Name: " + newName);
+        }
+
         textName.text = newName;
     }
 
