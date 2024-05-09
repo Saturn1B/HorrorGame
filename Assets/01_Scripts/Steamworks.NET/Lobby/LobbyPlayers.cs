25a5446e9f6057ff9b1c851126a231ed079117ed
Saussol <60229019+Saussol@users.noreply.github.com>
2024-05-09 12:00:24 +0200

    Modif lobby

--- Assets/01_Scripts/Steamworks.NET/Lobby/LobbyPlayers.cs
+++ Assets/01_Scripts/Steamworks.NET/Lobby/LobbyPlayers.cs
@@ -1,6 +1,7 @@
 using Mirror;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 
@@ -11,11 +12,34 @@ public class LobbyPlayers : NetworkBehaviour
     public GameObject[] players; //0 == host    1 2 3 == client
     public TextMeshProUGUI[] playersName;
 
+    public List<GameObject> playersObj = new List<GameObject>();
+
     public void Awake()
     {
         for (int i = 0; i < players.Length; i++)
         {
             players[i].SetActive(false);
+        }
+    }
+
+    public void FixedUpdate()
+    {
+        // Récupérer tous les objets avec le tag "Player"
+        GameObject[] playerObjects = GameObject.FindGameObjectsWithTag("Player");
+
+        // Ajouter les objets récupérés dans le HashSet
+        foreach (GameObject playerObject in playerObjects)
+        {
+            if (!playersObj.Contains(playerObject))
+            {
+                playersObj.Add(playerObject);                
+            }
+        }
+
+        for (int i = 0;i < playersObj.Count; i++)
+        {
+            players[i].SetActive(true);
+            playersName[i].text = playersObj[i].GetComponent<PlayerNameSync>().displayName;
         }
     }
 
