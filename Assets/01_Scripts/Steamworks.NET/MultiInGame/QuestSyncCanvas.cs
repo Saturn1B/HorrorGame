fae3c6eddaee8e2209a211452c2478fd4f647349
Saussol <60229019+Saussol@users.noreply.github.com>
2024-05-22 16:22:41 +0200

    Add quest text

--- Assets/01_Scripts/Steamworks.NET/MultiInGame/QuestSyncCanvas.cs
+++ Assets/01_Scripts/Steamworks.NET/MultiInGame/QuestSyncCanvas.cs
@@ -1,55 +1,141 @@
-
 using UnityEngine;
 using Mirror;
 using TMPro;  // Assurez-vous d'avoir TextMeshPro si vous l'utilisez
 
 public class QuestSyncCanvas : NetworkBehaviour
 {
-    [SerializeField] private TextMeshProUGUI sharedTextA;  // Utilisez TextMesh si vous utilisez TextMeshPro
+    [SerializeField] private TextMeshProUGUI sharedTextA;
     [SerializeField] private TextMeshProUGUI sharedTextB;
     [SerializeField] private TextMeshProUGUI sharedTextC;
     [SerializeField] private TextMeshProUGUI sharedTextD;
-    [SyncVar(hook = nameof(OnTextChanged))] private string syncedText;
+
+    [SyncVar(hook = nameof(OnTextChangedA))] private string syncedTextA;
+    [SyncVar(hook = nameof(OnTextChangedB))] private string syncedTextB;
+    [SyncVar(hook = nameof(OnTextChangedC))] private string syncedTextC;
+    [SyncVar(hook = nameof(OnTextChangedD))] private string syncedTextD;
+
+    public bool questAfinish = false;
 
     void Start()
     {
-        if (sharedTextA == null)
+        if (sharedTextA == null || sharedTextB == null || sharedTextC == null || sharedTextD == null)
         {
-            Debug.LogError("SharedText is not assigned!");
+            Debug.LogError("One or more SharedText fields are not assigned!");
             return;
         }
 
-        UpdateText("Pitot");
-   
+        UpdateTextA("Find 3 barrels");
+        UpdateTextB("Find 10 barrels");
+        UpdateTextC("Find 650 barrels");
+        UpdateTextD("Find 10000 barrels");
     }
 
     public void QuestA()
     {
-        UpdateText("Quest A Complete");
+        UpdateTextA("Quest A Complete");
     }
 
+    public void EndGame()
+    {
+        sharedTextB.gameObject.SetActive(false);
+        sharedTextC.gameObject.SetActive(false);
+        sharedTextD.gameObject.SetActive(false);
 
+        UpdateTextA("You need to go back to the boat");
+    }
 
-    public void UpdateText(string newText)
+    public void UpdateTextA(string newText)
     {
         if (isServer)
         {
-            syncedText = newText;
+            syncedTextA = newText;
         }
         else
         {
-            CmdUpdateText(newText);
+            CmdUpdateTextA(newText);
         }
     }
 
     [Command]
-    void CmdUpdateText(string newText)
+    void CmdUpdateTextA(string newText)
     {
-        syncedText = newText;
+        syncedTextA = newText;
     }
 
-    void OnTextChanged(string oldText, string newText)
+    void OnTextChangedA(string oldText, string newText)
     {
         sharedTextA.text = newText;
+    }
+
+    // Repeat for sharedTextB
+    public void UpdateTextB(string newText)
+    {
+        if (isServer)
+        {
+            syncedTextB = newText;
+        }
+        else
+        {
+            CmdUpdateTextB(newText);
+        }
+    }
+
+    [Command]
+    void CmdUpdateTextB(string newText)
+    {
+        syncedTextB = newText;
+    }
+
+    void OnTextChangedB(string oldText, string newText)
+    {
+        sharedTextB.text = newText;
+    }
+
+    // Repeat for sharedTextC
+    public void UpdateTextC(string newText)
+    {
+        if (isServer)
+        {
+            syncedTextC = newText;
+        }
+        else
+        {
+            CmdUpdateTextC(newText);
+        }
+    }
+
+    [Command]
+    void CmdUpdateTextC(string newText)
+    {
+        syncedTextC = newText;
+    }
+
+    void OnTextChangedC(string oldText, string newText)
+    {
+        sharedTextC.text = newText;
+    }
+
+    // Repeat for sharedTextD
+    public void UpdateTextD(string newText)
+    {
+        if (isServer)
+        {
+            syncedTextD = newText;
+        }
+        else
+        {
+            CmdUpdateTextD(newText);
+        }
+    }
+
+    [Command]
+    void CmdUpdateTextD(string newText)
+    {
+        syncedTextD = newText;
+    }
+
+    void OnTextChangedD(string oldText, string newText)
+    {
+        sharedTextD.text = newText;
     }
 }
